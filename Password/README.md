25a15601f32cc5f21cf15d5db46b89c12b14f6ff
petershaughnessy <prnessy23@gmail.com>
2022-12-22 17:12:07 -0800

    Added final code and comments in script.js, added comments and image to README.md file

--- Password/README.md
+++ Password/README.md
@@ -1,11 +1,19 @@
-### PASSWORD GENERATOR
+### PASSWORD-GENERATOR-HOMEWORK
 
-## Components used in creating this repository:
+---------------------------
+* Components referenced in creating this repository include those listed below however the starter HTML and CSS code that was included in the initial repository was left untouched and all of the changes made were in the script.js file:
 
-# HTML
-# CSS
-# Javascript
-# README.md
+        . HTML
+        . CSS
+        . Javascript
+        . README.md
+
+
+
+___
+
+
+ ![Alt text](Password.png)
 
 
 
--- Password/script.js
+++ Password/script.js
@@ -1,57 +1,143 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 // Creating arrays from which to draw on
-function generatePassword ()
+function generatePassword() {
+  var uppercaseArray = [
+    "A",
+    "B",
+    "C",
+    "D",
+    "E",
+    "F",
+    "G",
+    "H",
+    "I",
+    "J",
+    "K",
+    "L",
+    "M",
+    "N",
+    "O",
+    "P",
+    "Q",
+    "R",
+    "S",
+    "T",
+    "U",
+    "V",
+    "W",
+    "X",
+    "Y",
+    "Z",
+  ];
+  var lowercaseArray = [
+    "a",
+    "b",
+    "c",
+    "d",
+    "e",
+    "f",
+    "g",
+    "h",
+    "i",
+    "j",
+    "k",
+    "l",
+    "m",
+    "n",
+    "o",
+    "p",
+    "q",
+    "r",
+    "s",
+    "t",
+    "u",
+    "v",
+    "w",
+    "x",
+    "y",
+    "z",
+  ];
+  var numberArray = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+  var characterArray = [
+    "!",
+    "@",
+    "#",
+    "$",
+    "%",
+    "^",
+    "&",
+    "*",
+    "(",
+    ")",
+    "-",
+    "_",
+    "+",
+    "=",
+  ];
 
+  console.log(uppercaseArray[8]);
 
+  var resultArray = [];
+  var userArray = [];
 
+  var numbCharacter = prompt("Number of characters you want between 8 and 128");
 
-  var uppercaseArray = ["A","B","C","D","E","F","G","H","I","J",
-"K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
- var lowercaseArray = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
- var numberArray = ["0","1","2","3","4","5","6","7","8","9"];
- var characterArray =["!","@","#","$","%","^","&","*","(",")","-","_","+","="];
+  // Added while function to force user to choose between 8-128 characters and reminding them that they must
 
- var resultArray = [];
- var userArray = [];
+  while (numbCharacter < 8 || numbCharacter > 128) {
+    alert("You must be between 8 and 128");
+    numbCharacter = prompt("Number of characters you want between 8 and 128");
+  }
 
- uppercaseArray [1]
- var numbCharacter = prompt ("Number of characters you want between 8 and 128");
- var uppercaseArray = confirm ("Do you want Uppercases in your password");
- var lowercaseArray = confirm ("Do you want lower cases in your password");
- var numberArray = confirm ("Do you want numbers in your password");
- var characterArray = confirm ("Do you want special characters in your password");
+  var wantsUppercase = confirm("Do you want Uppercases in your password");
+  var wantsLowercase = confirm("Do you want lower cases in your password");
+  var wantsNumbers = confirm("Do you want numbers in your password");
+  var wantsSpecial = confirm("Do you want special characters in your password");
 
- //Condiitons for query to follow;
+    // Added while function to prevent user from choosing Cancel of every available option and forcing password to contain at least on of the arrays
 
- if (numbers){
-  resultArray =resultArray.concat(numberArray);
- }
- if (uppercaseArray){
-  resultArray =resultArray.concat(uppercaseArray);
- }
- if (lowercaseArray){ resultArray =resultArray.concat(lowercaseArray)};
- if (characterArray){resultArray = resultArray(characterArray)};
+  while (!uppercaseArray && !lowercaseArray && !numberArray && !characterArray) {
+    alert("you must choose at least one type of character")
+    wantsUppercase = confirm("Do you want Uppercases in your password");
+    wantsLowercase = confirm("Do you want lower cases in your password");
+    wantsNumbers = confirm("Do you want numbers in your password");
+    wantsSpecial = confirm("Do you want special characters in your");
+  };
 
- console.log(resultArray)
+  //Condiitons for query to follow;
 
- for (var i = 0; i < numbCharacter; i++) {
-  userArray.push (resultArray)[Math.floor(Math.random() * resultArray.length)]}
+  if (wantsUppercase) {
+    resultArray = resultArray.concat(uppercaseArray);
+  }
+  if (wantsLowercase) {
+    resultArray = resultArray.concat(lowercaseArray);
+  }
+  if (wantsSpecial) {
+    resultArray = resultArray.concat(characterArray);
+  }
+  if (wantsNumbers) {
+    resultArray = resultArray.concat(numberArray);
+  }
 
-  return userArray.join('');
+  for (var i = 0; i < numbCharacter; i++) {
+    userArray += resultArray[Math.floor(Math.random() * resultArray.length)]
+  }
 
-  // Write password to the #password input
+  return userArray;
+}
+
+// Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
-function copytoClipboard(){
+
+function copytoClipboard() {
   // Bonus
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-}
